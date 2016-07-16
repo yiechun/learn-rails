21bcef8c6d25d092d87b11fe0c7900106caad09b
yiechun <yiechun@gmail.com>
2016-07-16 23:13:32 +0800

    dynamic home page

---
+++ app/models/owner.rb
@@ -0,0 +1,21 @@
+class Owner
+
+	def name
+		name = 'Chowdao'
+	end
+
+	def birthdate
+		birthdate = Date.new(1989, 01, 26)
+	end
+
+	def countdown
+		today = Date.today
+		birthday = Date.new(today.year,birthdate.month,birthdate.day)
+		if birthday > today
+			countdown = (birthday - today).to_i
+		else
+			countdown = (birthday.next_year - today).to_i
+		end
+	end
+	
+end
