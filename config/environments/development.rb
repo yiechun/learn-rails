2264309dfb19efc3f3f07a07fb40efcc27a57e02
yiechun <yiechun@gmail.com>
2016-07-16 17:35:29 +0800

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,14 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
+  config.action_mailer.smtp_settings = { address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["DOMAIN_NAME"], 
+    authentication: "plain", 
+    enable_starttls_auto: true, 
+    user_name: ENV["GMAIL_USERNAME"], 
+    password: ENV["GMAIL_PASSWORD"]
+  }
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
